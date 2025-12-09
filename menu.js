abbb2a20903b8d12f5caf8cb3726783a1b970978
caro8847 <carolinestiepernielsen@gmail.com>
2025-12-09 20:49:09 +0100

    burger-menu js+css

---
+++ menu.js
@@ -0,0 +1,27 @@
+//Caroline
+//https://www.youtube.com/watch?v=HbBMp6yUXO0 - udførelse af burger-menu
+//https://chatgpt.com/share/69387a5d-bdc8-800a-92fc-97ed55f5c0e8 - animation
+
+console.log("It's working")
+
+const navToggle = document.querySelector('.mobile-nav-toggle');
+const primaryNav = document.querySelector('.header__menu');
+const burgerLines = document.querySelectorAll('.burger-line');
+
+navToggle.addEventListener('click', () => {
+    const visibility = primaryNav.getAttribute('data-visible');
+
+    if (visibility === "false") {
+        primaryNav.setAttribute('data-visible', "true");
+        // Tilføj kryds-animation med for-loop
+        for (let i = 0; i < burgerLines.length; i++) {
+            burgerLines[i].classList.add('cross');
+        }
+    } else {
+        primaryNav.setAttribute('data-visible', "false");
+        // Fjern kryds-animation
+        for (let i = 0; i < burgerLines.length; i++) {
+            burgerLines[i].classList.remove('cross');
+        }
+    }
+});
